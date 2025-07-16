e62ca27c6eefcceeea08661b6336e08a4869a09d
bladi <bladi.fn@gmail.com>
2025-07-15 22:36:32 -0400

    Back-End de TiendaLaptop

---
+++ WebApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entidades;
+
+namespace WebApi.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Laptop> Laptops {  get; set; }
+ 
+       
+    }
+}
